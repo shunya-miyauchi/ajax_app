ebad13186c18b3f2b3357781b3b2e445c6cc4f90
Miyauchi <hy.miyauchi@gmail.com>
2022-03-18 15:24:37 +0900

    [add]commentモデル作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
   resources :users, only: [:new,:create,:show]
   resources :sessions, only: [:new,:create,:destroy]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_18_053026) do
+ActiveRecord::Schema.define(version: 2022_03_18_062203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 2022_03_18_053026) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "comments", force: :cascade do |t|
+    t.bigint "blog_id", null: false
+    t.text "content"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["blog_id"], name: "index_comments_on_blog_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email"
@@ -29,4 +37,5 @@ ActiveRecord::Schema.define(version: 2022_03_18_053026) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "comments", "blogs"
 end
