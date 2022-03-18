1ad4caeace459077323e29f93eaf9f5599970a06
Miyauchi <hy.miyauchi@gmail.com>
2022-03-18 14:50:01 +0900

    [fix]usersコントローラ、ビュー

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,20 @@
 class UsersController < ApplicationController
+
+  def new
+    @users = User.all
+  end
+
+  def create
+    @user = User.new
+    if @user.save
+      redirect_to blogs_path
+    else
+      render:new
+    end
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+
 end
