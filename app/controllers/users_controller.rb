e8cd51dc62b79402c74aef70a1dd9ca3520e4a96
Miyauchi <hy.miyauchi@gmail.com>
2022-03-18 15:09:00 +0900

    [fix]users_strongパラメーター追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,11 @@
 class UsersController < ApplicationController
 
   def new
-    @users = User.all
+    @user = User.new
   end
 
   def create
-    @user = User.new
+    @user = User.new(user_params)
     if @user.save
       redirect_to blogs_path
     else
@@ -15,6 +15,12 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email)
   end
 
 end
