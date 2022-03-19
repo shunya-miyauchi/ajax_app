1f1f3738d5070a672b2ce58fd553334d9f7a81e9
Miyauchi <hy.miyauchi@gmail.com>
2022-03-19 23:09:12 +0900

    1対1メッセージ実装

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index,:new,:create,:show]
   resources :sessions, only: [:new,:create,:destroy]
   resources :relationships, only: [:create, :destroy]
+  resources :conversations do
+    resources :messages
+  end
 end
