f3dfabe1748a7e394576473b27cffcfba9cd1a7e
Miyauchi <hy.miyauchi@gmail.com>
2022-03-19 22:05:11 +0900

    友達つながり実装

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :blogs do
     resources :comments
   end
-  resources :users, only: [:new,:create,:show]
+  resources :users, only: [:index,:new,:create,:show]
   resources :sessions, only: [:new,:create,:destroy]
+  resources :relationships, only: [:create, :destroy]
 end
